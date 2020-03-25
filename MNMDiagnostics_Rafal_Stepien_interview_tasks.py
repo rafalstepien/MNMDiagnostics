142a846e9af8a0e72ff1c03899ddae9dbf236fb0
Rafal Stepien <rstepien095@gmail.com>
2020-03-25 13:39:12 +0100

    Task 2 done

--- MNMDiagnostics_Rafal_Stepien_interview_tasks.py
+++ MNMDiagnostics_Rafal_Stepien_interview_tasks.py
@@ -1,5 +1,8 @@
 #!bin/python3
 
+from collections import Counter
+
+# ------------------------------- TASK 1 -------------------------------------------
 
 def change_letters_size(input_string):
     """
@@ -16,5 +19,19 @@ def change_letters_size(input_string):
             str_2 += letter.upper()
     return [str_1, str_2]
 
+
+
+# ------------------------------- TASK 2 -------------------------------------------
+
+def get_number_of_redundant(input_string):
+    main = Counter(list(input_string.lower()))
+    more_than_once = []
+    for letter, occurrences in main.items():
+    	if occurrences > 1:
+    		more_than_once.append(letter)
+    return len(more_than_once)
+
+
 if __name__ == "__main__":
     print(change_letters_size("abcdef"))
+    print(get_number_of_redundant('abba'))
