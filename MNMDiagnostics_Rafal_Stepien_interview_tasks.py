5fdb233e05f8d5b0364a59654aafdbc2a4ce5c50
Rafal Stepien <rstepien095@gmail.com>
2020-03-25 16:59:01 +0100

    Add Task 5

--- MNMDiagnostics_Rafal_Stepien_interview_tasks.py
+++ MNMDiagnostics_Rafal_Stepien_interview_tasks.py
@@ -41,27 +41,50 @@
 
 # ------------------------------- TASK 3 -------------------------------------------
 
+# import gzip
+
+# def subset_vcf(filename):
+# 	with gzip.open(filename, 'rb') as f:
+# 		new_vcf = ''
+# 		header = ''
+# 		for line in f:
+# 			line = line.decode("utf-8")
+# 			if "#" not in line:
+# 				line = line.split("\t")
+# 				if (line[0] == "12") & (int(line[1]) >= 112204691) & (int(line[1]) <= 112247789):
+# 					new_vcf += '\t'.join(line) + "\n"
+# 			else:
+# 				header += line
+# 	content = header+new_vcf
+# 	save(filename="chrom_12.vcf", content=content)
+
+# def save(filename, content):
+# 	with open(filename, "w") as file:
+# 		file.write(content)
+
+# subset_vcf('CPCT02220079.annotated.processed.vcf.gz')
+
+# ------------------------------- TASK 5 -------------------------------------------
 import gzip
 
-def subset_vcf(filename):
+def count_allele_frequency(filename):
 	with gzip.open(filename, 'rb') as f:
-		new_vcf = ''
-		header = ''
+		counter = 0
 		for line in f:
-			line = line.decode("utf-8")
-			if "#" not in line:
-				line = line.split("\t")
-				if (line[0] == "12") & (int(line[1]) >= 112204691) & (int(line[1]) <= 112247789):
-					new_vcf += '\t'.join(line) + "\n"
-			else:
-				header += line
-	content = header+new_vcf
-	save(filename="chrom_12.vcf", content=content)
+			line = line.decode("utf-8").split("\t")
+			if ("#" not in line[0]):
+				if line[6] == "PASS":
+					genotype = line[9].split(":")[0]
+					if genotype == "0/1":
+						try:
+							tmp = line[7].split("GoNLv5_AF=")[1]
+							allele_frequency = tmp.split(";")[0]
+						except IndexError:
+							allele_frequency = 999
+						if float(allele_frequency) < 0.01:
+							counter += 1
+	return counter
 
-def save(filename, content):
-	with open(filename, "w") as file:
-		file.write(content)
 
-subset_vcf('CPCT02220079.annotated.processed.vcf.gz')
-
-	
+print(count_allele_frequency('CPCT02220079.annotated.processed.vcf.gz'))
+				
